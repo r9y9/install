6a00ebc3d99d1f4cec189b0957d9ca6f63e6070e
Ryuichi Yamamoto <zryuichi@gmail.com>
2013-08-11 18:24:29 +0900

    [emacs] modify

--- install_emacs.sh
+++ install_emacs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# It only works on 'apt-get' equipped system such as Ubuntu and Linux mint
 function install_emacs24() {
     sudo add-apt-repository ppa:cassou/emacs
     sudo apt-get update
